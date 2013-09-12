f5e3d0fb6ef17d6e65df90462112f187ab94ad0f
Isura <isura@ieee.org>
2013-09-12 06:59:15 -0500

    Lower higher lambda

--- uta_pr2_forceControl/src/neuroadptController.cpp
+++ uta_pr2_forceControl/src/neuroadptController.cpp
@@ -255,8 +255,8 @@ bool PR2NeuroadptControllerClass::init( pr2_mechanism_model::RobotState *robot, 
 	// NN
 
 	kappa  = 0.07;
-	Kv     = 10; // prop. gain for PID inner loop
-	lambda = 0.1; //*std::sqrt(Kp); // der. gain for PID inner loop
+	Kv     = 30; // prop. gain for PID inner loop
+	lambda = 0.01; //*std::sqrt(Kp); // der. gain for PID inner loop
 	Kz     = 3;
 	Zb     = 100;
 
