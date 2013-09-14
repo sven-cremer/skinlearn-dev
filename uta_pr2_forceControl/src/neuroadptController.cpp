a52379712eb0500ce2e65a112bae03654c15d5e8
Isura <isura@ieee.org>
2013-09-14 17:58:48 -0500

    Flipped FT x sign

--- uta_pr2_forceControl/src/neuroadptController.cpp
+++ uta_pr2_forceControl/src/neuroadptController.cpp
@@ -397,12 +397,12 @@ void PR2NeuroadptControllerClass::update()
 	std::vector<geometry_msgs::Wrench> r_ftData_vector = r_ft_handle_->state_.samples_;
 	r_ft_samples    = r_ftData_vector.size() - 1;
 //      r_ftData.wrench = r_ftData_vector[r_ft_samples];
-	r_ftData.wrench.force.x  = r_ftData_vector[r_ft_samples].force.x  - r_ftBias.wrench.force.x ;
-	r_ftData.wrench.force.y  = r_ftData_vector[r_ft_samples].force.y  - r_ftBias.wrench.force.y ;
-	r_ftData.wrench.force.z  = r_ftData_vector[r_ft_samples].force.z  - r_ftBias.wrench.force.z ;
-	r_ftData.wrench.torque.x = r_ftData_vector[r_ft_samples].torque.x - r_ftBias.wrench.torque.x;
-	r_ftData.wrench.torque.y = r_ftData_vector[r_ft_samples].torque.y - r_ftBias.wrench.torque.y;
-	r_ftData.wrench.torque.z = r_ftData_vector[r_ft_samples].torque.z - r_ftBias.wrench.torque.z;
+	r_ftData.wrench.force.x  = - ( r_ftData_vector[r_ft_samples].force.x  - r_ftBias.wrench.force.x ) ;
+	r_ftData.wrench.force.y  =     r_ftData_vector[r_ft_samples].force.y  - r_ftBias.wrench.force.y   ;
+	r_ftData.wrench.force.z  =     r_ftData_vector[r_ft_samples].force.z  - r_ftBias.wrench.force.z   ;
+	r_ftData.wrench.torque.x =     r_ftData_vector[r_ft_samples].torque.x - r_ftBias.wrench.torque.x  ;
+	r_ftData.wrench.torque.y =     r_ftData_vector[r_ft_samples].torque.y - r_ftBias.wrench.torque.y  ;
+	r_ftData.wrench.torque.z =     r_ftData_vector[r_ft_samples].torque.z - r_ftBias.wrench.torque.z  ;
 
 //	if( (r_ftData.wrench.force.x > -18) && (r_ftData.wrench.force.x < 18) ){ r_ftData.wrench.force.x = 0; }
 //	if( (r_ftData.wrench.force.y > -18) && (r_ftData.wrench.force.y < 18) ){ r_ftData.wrench.force.y = 0; }
