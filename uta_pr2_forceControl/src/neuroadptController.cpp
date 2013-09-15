ee46fd7c0e0b28b2224fc3942cb63d4e793b6d55
Isura <isura@ieee.org>
2013-09-15 00:38:25 -0500

    Debug

--- uta_pr2_forceControl/src/neuroadptController.cpp
+++ uta_pr2_forceControl/src/neuroadptController.cpp
@@ -598,22 +598,22 @@ void PR2NeuroadptControllerClass::update()
 	// System Model END
 	/////////////////////////
 
-//	// DEBUG
-//	q_m(0)  = 0 ; //- 0.5 * (sin(circle_phase_) + 1 );
-//	q_m(1)  = 0 ; //- 0.5 * (sin(circle_phase_) + 1 );
-//	q_m(2)  = 0 ; //- 0.5 * (sin(circle_phase_) + 1 );
-//	q_m(3)  =       - 0.5 * (sin(circle_phase_) + 1.5 );
-//	q_m(4)  = 0;
-//	q_m(5)  = 0;
-//	q_m(6)  = 0;
-//
-//	qd_m(0) = 0;
-//	qd_m(1) = 0;
-//	qd_m(2) = 0;
-//	qd_m(3) = - 10 * (cos(circle_phase_));
-//	qd_m(4) = 0;
-//	qd_m(5) = 0;
-//	qd_m(6) = 0;
+	// DEBUG
+	q_m(0)  = 0 ; //- 0.5 * (sin(circle_phase_) + 1 );
+	q_m(1)  = 0 ; //- 0.5 * (sin(circle_phase_) + 1 );
+	q_m(2)  = 0 ; //- 0.5 * (sin(circle_phase_) + 1 );
+	q_m(3)  =       - 0.5 * (sin(circle_phase_) + 1.5 );
+	q_m(4)  = 0;
+	q_m(5)  = 0;
+	q_m(6)  = 0;
+
+	qd_m(0) = 0;
+	qd_m(1) = 0;
+	qd_m(2) = 0;
+	qd_m(3) = - 10 * (cos(circle_phase_));
+	qd_m(4) = 0;
+	qd_m(5) = 0;
+	qd_m(6) = 0;
 
 
     /////////////////////////
