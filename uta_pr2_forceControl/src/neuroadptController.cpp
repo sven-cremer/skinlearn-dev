8b50948683f11f751f78643543f3ec620b5abda7
Isura Ranatunga <isura@ieee.org>
2014-03-11 17:09:36 -0500

    Default no motion

--- uta_pr2_forceControl/src/neuroadptController.cpp
+++ uta_pr2_forceControl/src/neuroadptController.cpp
@@ -1159,7 +1159,7 @@ bool PR2NeuroadptControllerClass::capture( std_srvs::Empty::Request& req,
   ros::Time started = ros::Time::now();
 
   // Start circle traj
-  startCircleTraj = true;
+  startCircleTraj = false;
 
   /* Mark the buffer as clear (which will start storing). */
   storage_index_ = 0;
