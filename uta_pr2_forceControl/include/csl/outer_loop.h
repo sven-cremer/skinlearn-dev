a8a67d3f47e426716db6123b64a12e21d54008f1
Isura <isura@ieee.org>
2014-04-05 18:23:10 -0500

    Previous edit didnt fix issue

--- uta_pr2_forceControl/include/csl/outer_loop.h
+++ uta_pr2_forceControl/include/csl/outer_loop.h
@@ -439,17 +439,17 @@ public:
   void update()
   {
 
-    ode_init_x[2] = f_r(0);
+//    ode_init_x[2] = f_r(0);
+//
+//    boost::numeric::odeint::integrate( oneDmsd_model , ode_init_x , 0.0 , delT , delT );
+//
+//    x_m  (0) = ode_init_x[0 ] ;
+//    xd_m (0) = ode_init_x[1 ] ;
+//    xdd_m(0) = m*( ode_init_x[2] - d*ode_init_x[1 ] - k*ode_init_x[0 ] );
 
-    boost::numeric::odeint::integrate( oneDmsd_model , ode_init_x , 0.0 , delT , delT );
-
-    x_m  (0) = ode_init_x[0 ] ;
-    xd_m (0) = ode_init_x[1 ] ;
-    xdd_m(0) = m*( ode_init_x[2] - d*ode_init_x[1 ] - k*ode_init_x[0 ] );
-
-//    x_m   = x_m  + xd_m *delT ;
-//    xd_m  = xd_m + xdd_m*delT ;
-//    xdd_m = ( f_r - d*xd_m - k*x_m )/m;
+    x_m   = x_m  + xd_m *delT ;
+    xd_m  = xd_m + xdd_m*delT ;
+    xdd_m = ( f_r - d*xd_m - k*x_m )/m;
 
   }
 };
