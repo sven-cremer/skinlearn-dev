c95a146ebccf94f7ad1aee56fc598d30747d28e3
sven-uta <sven.cremer@mavs.uta.edu>
2016-03-21 15:14:35 -0500

    Fixed sign issue when setting X_m to x0_cali_vec.

--- ice_robot_controllers/src/adaptNeuroController.cpp
+++ ice_robot_controllers/src/adaptNeuroController.cpp
@@ -529,7 +529,7 @@ void PR2adaptNeuroControllerClass::updateNonRealtime()
 				if(tmp(2)<2)
 					tmp(2)=-2;
 
-				X_m.topRows(3) = x0_cali_vec_.topRows(3).cwiseProduct(Vec3d_ones - sensorDirections.col(tactileSensorSelected_));
+				X_m.topRows(3) = x0_cali_vec_.topRows(3).cwiseProduct(Vec3d_ones - sensorDirections.col(tactileSensorSelected_).cwiseAbs());
 				X_m.topRows(3) += tmp(1)*sensorDirections.col(tactileSensorSelected_);
 //				Xd_m .topRows(3) = tmp(2)*sensorDirections.col(tactileSensorSelected_); // or tmp(0)/delT   ?
 //				Xdd_m.topRows(3) += tmp(3)*sensorDirections.col(tactileSensorSelected_); // or tmp(0)/delT^2 ?		// FIXME
